8160d240a1d802fbd08b5eec355ea1de8ae74e6b
Bin, Huang <30388171+beenhuang@users.noreply.github.com>
2023-03-08 12:49:16 +0000

    Add files via upload

---
+++ attack/cumul/extract.py
@@ -0,0 +1,147 @@
+#!/usr/bin/env python3
+
+"""
+<file>    extract.py
+<brief>   extract the CUMUL feature
+"""
+
+import os
+import sys
+import pickle
+import argparse
+import logging
+import numpy as np
+import multiprocessing as mp
+from os.path import abspath, dirname, join, pardir, splitext, basename, exists
+
+from cumul_feature import cumul_feature
+
+#PACKET_SIZE = 514
+PACKET_SIZE = 1
+
+# create logger
+def get_logger():
+    logging.basicConfig(format="[%(asctime)s]>>> %(message)s", level=logging.INFO, datefmt = "%Y-%m-%d %H:%M:%S")
+    logger = logging.getLogger(splitext(basename(__file__))[0])
+    
+    return logger
+
+# parse arugment
+def parse_arguments():
+    # argument parser
+    parser = argparse.ArgumentParser(description="CUMUL")
+
+    # INPUT
+    parser.add_argument("-i", "--in", required=True, help="load trace data")
+
+    args = vars(parser.parse_args())
+
+    return args
+
+def preprocess(trace):
+    good_trace = []
+
+    for e in trace:
+        # in cumul, positive is incoming when positive of Wang's Data is outgoing
+        size = int(e.split("\t")[1].strip("\n")) * PACKET_SIZE * -1
+
+        good_trace.append(size)
+
+    #print(f"good_trace: {good_trace}")
+
+    return good_trace
+
+def extract(data_dir, feature_dir, file, umon_label=100):
+    # read a trace file.
+    file_path = join(data_dir, file)
+    with open(file_path, "r") as f:
+        trace = f.readlines() 
+
+    # preprocess the trace.    
+    good_trace = preprocess(trace)  
+    #times, sizes = preprocess2(trace)
+
+    # get feature vector
+    feature = cumul_feature(good_trace)
+
+    # get the trace file name
+    feature_fname = file.split(".")[0]
+    print(f"feature_fname: {feature_fname}")
+    
+    # get label
+    if "-" in feature_fname:
+        label = int(feature_fname.split("-")[0])
+    else:
+        label = umon_label
+
+    
+    # save the feature from the trace
+    feature_path = join(feature_dir, feature_fname)
+    with open(feature_path, "w") as f:
+        for element in feature:
+            f.write(f"{element}\n")
+
+    return (feature, label)   
+
+# extract features one by one.
+def main2(data_dir, feature_dir, feature_pickle="feature.pkl"):
+    flist = os.listdir(data_dir)
+
+    X, y = [], []
+    for file in tqdm(flist, total=len(flist)):
+        feature, label = extract(data_dir, feature_dir, file)
+
+        X.append(feature)
+        y.append(label)
+
+    with open(join(feature_dir, feature_pickle), "wb") as f:
+        pickle.dump((X, y), f)    
+    
+    logger.info(f"Complete")  
+
+# use multiprocessing
+def main(data_dir, feature_dir, feature_pickle="feature.pkl"):
+    flist = os.listdir(data_dir)
+    params = [[data_dir, feature_dir, f] for f in flist]
+
+    with mp.Pool(mp.cpu_count()) as pool:
+        result = pool.starmap(extract, params)
+
+    X, y = zip(*result) 
+
+    with open(join(feature_dir, feature_pickle), "wb") as f:
+        pickle.dump((X, y), f)    
+    
+    #logger.debug(f"X: {X}")
+    #logger.debug(f"y: {y}")
+    
+    logger.info(f"Complete")     
+
+
+
+if __name__ == "__main__":
+    MODULE_NAME = basename(__file__)
+
+    BASE_DIR = abspath(join(dirname(__file__), pardir, pardir))
+    IN_DATA_DIR = join(BASE_DIR, "data")
+    MAIN_FEATURE_DIR = join(BASE_DIR, "attack", "cumul", "feature")
+
+    try:
+        logger = get_logger()
+        logger.info(f"{MODULE_NAME}: start to run.")
+
+        # parse arguments
+        args = parse_arguments()
+        logger.info(f"Arguments: {args}")
+
+        data = join(IN_DATA_DIR, args["in"])
+
+        feature_dir = join(MAIN_FEATURE_DIR, args["in"])
+        if not exists(feature_dir):
+            os.makedirs(feature_dir)
+
+        main(data, feature_dir)    
+
+
+    except KeyboardInterrupt:
+        sys.exit(-1) 
---
+++ attack/cumul/svm_classifier.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python3
+
+"""
+<file>    svm_classifier.py
+<brief>   svm classifier used by CUMUL
+"""
+
+from sklearn.svm import SVC
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+
+from sklearn.model_selection import train_test_split, cross_val_score, GridSearchCV
+from sklearn.metrics import make_scorer
+
+# training
+def train_cumul(X_train, y_train, ker="rbf", c=2048, g=0.015625):
+    model = Pipeline([("standardscaler", StandardScaler()), ("svc", SVC(kernel=ker, C=c, gamma=g))])
+    model.fit(X_train, y_train)
+
+    return model
+
+# test
+def test_cumul(model, X_test):
+    y_pred = model.predict(X_test)
+
+    return y_pred
+
+
+# get open-world score
+def get_openworld_score(y_true, y_pred, label_unmon):
+    # TP-correct, TP-incorrect, FN  TN, FN
+    tp_c, tp_i, fn, tn, fp = 0, 0, 0, 0, 0
+
+    #print(f"label_unmon: {label_unmon}")
+
+    # traverse preditions
+    for i in range(len(y_pred)):
+        # [case_1]: positive sample, and predict positive and correct.
+        if y_true[i] != label_unmon and y_pred[i] != label_unmon and y_pred[i] == y_true[i]:
+            tp_c += 1
+        # [case_2]: positive sample, predict positive but incorrect class.
+        elif y_true[i] != label_unmon and y_pred[i] != label_unmon and y_pred[i] != y_true[i]:
+            tp_i += 1
+        # [case_3]: positive sample, predict negative.
+        elif y_true[i] != label_unmon and y_pred[i] == label_unmon:
+            fn += 1
+        # [case_4]: negative sample, predict negative.    
+        elif y_true[i] == label_unmon and y_pred[i] == y_true[i]:
+            tn += 1
+        # [case_5]: negative sample, predict positive    
+        elif y_true[i] == label_unmon and y_pred[i] != y_true[i]:
+            fp += 1   
+        else:
+            sys.exit(f"[ERROR]: {y_pred[i]}, {y_true[i]}")        
+
+    # accuracy
+    accuracy = (tp_c+tn) / float(tp_c+tp_i+fn+tn+fp)
+    # precision      
+    precision = tp_c / float(tp_c+tp_i+fp)
+    # recall or TPR
+    recall = tp_c / float(tp_c+tp_i+fn)
+    # F1-score
+    f1 = 2*(precision*recall) / float(precision+recall)
+    # FPR
+    fpr = fp / float(fp+tn)
+
+    lines = []
+    lines.append(f"[POS] TP-c: {tp_c},  TP-i(incorrect class): {tp_i},  FN: {fn}\n")
+    lines.append(f"[NEG] TN: {tn},  FP: {fp}\n\n")
+    lines.append(f"accuracy: {accuracy}\n")
+    lines.append(f"precision: {precision}\n")
+    lines.append(f"recall: {recall}\n")
+    lines.append(f"F1: {f1}\n")
+    lines.append(f"TPR: {recall}\n")
+    lines.append(f"FPR: {fpr}\n\n\n")
+
+    return lines
+
+
+## the follow functions are used for finding optimal parameters ##
+
+# in order to find the optimal hyperparameters
+# c: 2**11 ~ 2**17
+# gamma: 2**-3 ~ 2**3
+def find_optimal_hyperparams(X, y):
+    #
+    model = Pipeline([("standardscaler", StandardScaler()), ("svc", SVC(kernel="rbf"))])
+    #hyperparams = {"svc__C":[2048, 4096, 8192, 16384, 32768, 65536, 131072], "svc__gamma":[0.125, 0.25, 0.5, 0, 2, 4, 8]}
+    hyperparams = {"svc__C":[256, 526, 1024, 2048], "svc__gamma":[0.0078125, 0.015625, 0.03125, 0.0625]}
+    
+    clf = GridSearchCV(model, hyperparams, n_jobs=-1, cv=10, scoring=make_scorer(openworld_recall_score, label_unmon=max(y)))
+    clf.fit(X, y)
+    
+    # write to txt file
+    lines = []
+    lines.append(f"best_params_: {clf.best_params_} \n")
+    lines.append(f"best_score_: {clf.best_score_} ")
+
+    return lines
+
+# recall score used by find_optimal_hyperparams() and get_cross_val_score()
+def openworld_recall_score(y_true, y_pred, label_unmon):
+    # TP-correct, TP-incorrect, FN  TN, FP
+    tp_c, tp_i, fn, tn, fp = 0, 0, 0, 0, 0
+    
+    #logger.debug(f"label_unmon: {label_unmon}")
+
+    # traverse preditions
+    for i in range(len(y_pred)):
+        # [case_1]: positive sample, and predict positive and correct.
+        if y_true[i] != label_unmon and y_pred[i] != label_unmon and y_pred[i] == y_true[i]:
+            tp_c += 1
+        # [case_2]: positive sample, predict positive but incorrect class.
+        elif y_true[i] != label_unmon and y_pred[i] != label_unmon and y_pred[i] != y_true[i]:
+            tp_i += 1
+        # [case_3]: positive sample, predict negative.
+        elif y_true[i] != label_unmon and y_pred[i] == label_unmon:
+            fn += 1
+        # [case_4]: negative sample, predict negative.    
+        elif y_true[i] == label_unmon and y_pred[i] == y_true[i]:
+            tn += 1
+        # [case_5]: negative sample, predict positive    
+        elif y_true[i] == label_unmon and y_pred[i] != y_true[i]:
+            fp += 1   
+        else:
+            sys.exit(f"[ERROR]: {y_pred[i]}, {y_true[i]}")        
+
+
+    # recall
+    recall = tp_c / float(tp_c+tp_i+fn)
+
+    return recall
+
+
+
